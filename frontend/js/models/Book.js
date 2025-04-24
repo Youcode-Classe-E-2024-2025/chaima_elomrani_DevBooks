3ec6209950a9a704b5bb3ee2b6e63dcc951124bf
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-24 02:07:09 +0100

    added the class of books

---
+++ frontend/js/models/Book.js
@@ -0,0 +1,12 @@
+class Books {
+    constructor(id, title, description , auteur , category_id , read_status , dispo_status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.auteur = auteur;
+        this.category_id = category_id;
+        this.read_status = read_status;
+        this.dispo_status = dispo_status;
+    }
+
+}
