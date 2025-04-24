d882816cfa3827110b33de9bf98a397bf6b95b1e
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-24 02:16:11 +0100

    added the users class

---
+++ frontend/js/models/Users.js
@@ -0,0 +1,9 @@
+class users {
+    constructor(id, username,email, password, role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
