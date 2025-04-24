1ff0398596d7ec81f0fd1980249686632c6a8d44
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-24 02:13:56 +0100

    added the borrows class

---
+++ frontend/js/models/Borrows.js
@@ -0,0 +1,11 @@
+class Borrows {
+    constructor(id ,user_id, book_id, date_emprunt , date_retour, date_limit){
+        this.id = id;
+        this.user_id = user_id;
+        this.book_id = book_id;
+        this.date_emprunt = date_emprunt;
+        this.date_retour = date_retour;
+        this.date_limit = date_limit;
+    };
+
+}
